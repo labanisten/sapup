7cdd9cc8993ef93af9d73fe0326e80784e38a9d5
Jan Ingar <jan.ingart@gmail.com>
2012-06-13 12:09:20 +0200

    displaying of data

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,24 +1,80 @@
 function TimelineCtrl($scope) {
   $scope.statuslines = [
-    {system:'P03', start:'20120516', end:'20120523', status:'available'}, 
-    {system:'P10', start:'20120512', end:'20120520', status:'freeze', colsp:'20'},
+    //{system:'P03', start:'20120516', end:'20120523', status:'available'}, 
+    {system:'P10', start:'20120503', end:'20120510', status:'freeze', colsp:'20'},
     {system:'P10', start:'20120512', end:'20120520', status:'freeze'},
-	{system:'P11', start:'20120512', end:'20120520', status:'freeze'},
-	{system:'P11', start:'20120512', end:'20120520', status:'freeze'}];
+	{system:'P11', start:'20120510', end:'20120512', status:'freeze'},
+	{system:'P11', start:'20120515', end:'20120530', status:'freeze'}];
+	
+	
+    $scope.systemlines = [
+		{system:'P03', statuslines:[
+									{start:'20120501', end:'20120502', status:'freeze'},
+									{start:'20120503', end:'20120510', status:'available'},
+									{start:'20120511', end:'20120520', status:'freeze'},
+									{start:'20120521', end:'20120530', status:'available'},
+								   ]
+		},
+		
+		{system:'P10', statuslines:[
+									{start:'20120501', end:'20120525', status:'available'},
+									{start:'20120526', end:'20120530', status:'freeze'}
+								   ]
+		},
+		
+		{system:'P11', statuslines:[
+									{start:'20120501', end:'20120510', status:'freeze'},
+									{start:'20120511', end:'20120525', status:'available'},
+									{start:'20120526', end:'20120530', status:'freeze'}
+								   ]
+		}
+	];
+	
  
   $scope.addTimeline = function() {
     $scope.statuslines.push({system:$scope.system, start:$scope.start, end:$scope.end, status:$scope.status});
   };
   
   $scope.getStatusLines = function() {
-	//var list = [];	
+	//var list = [];
 	//list.push({'system':'P20', 'status':'available'});
 	//$scope.statuslines = $.merge($scope.statuslines, list);
-	//$scope.statuslines.push({system:'P30', start:'20120512', end:'20120520', status:'freeze'});
-	//$scope.statuslines = $.unique($scope.statuslines);
+	
+	//var unique_values = {};
+	//var list_of_values = [];
+	
+    //var list = $scope.statuslines.concat({system:'P30', start:'20120512', end:'20120520', status:'freeze'});
+	//var list = $.merge($scope.statuslines, {'system':'P30', 'start':'20120512', 'end':'20120520', 'status':'freeze'});
+	
+	//list = $.unique(list);
+	/*
+	$.each(
+		list, 
+		function(item){
 
-	return $scope.statuslines;
+		}
+	);
+	*/
+	
+	var list = $scope.systemlines;
+	//list = list.concat({system:'P30', start:'20120512', end:'20120520', status:'freeze'});
+	//list = $.unique(list);
+	
+    $.each(
+		list, 
+		function(item, val){
+			console.log(item + ' ' + val.system + ' ' + val.statuslines[0].start);
+		}
+	);
+	
+	console.log(list.length);
+	
+	
 
+	//list = $.merge(list, {'system':'P30', 'start':'20120512', 'end':'20120520', 'status':'freeze'});
+	
+	return list;
+	
   };
  
 }
