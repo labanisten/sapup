9dc6edf6ade4e029c6512ea0c8eca1d9179aaa65
Jingart <jan.ingart@gmail.com>
2012-06-15 14:47:32 +0200

    calendarelement spacing

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,10 +2,62 @@
 
 var myModule = angular.module('systemAvailability', ['mongolab']);
 
-
 function TimelineCtrl($scope, Systems) {
 
+	
+	function custom_sort(a, b) {
+		//return new Date(a.start).getTime() - new Date(b.start).getTime();
+		return a.start - b.start;
+	}
 
+
+	function getDBdata(){
+
+		// TODO: sorting 
+
+		var syslines = Systems.query(function(){
+
+			$.each(syslines, function(i, v_system) {
+			
+				$.each(v_system.statuslines, function(j, v_status) {
+
+					if(j == 0 && v_status.start > '20120501'){
+					
+						syslines[i].statuslines.unshift({"start":"20120501", "end":""+(v_status.start - 1)+"", "status":"available"});
+						
+					}else if( j > 0 && v_status.start - 1 != (v_system.statuslines[j - 1].end)){
+					
+						//TODO: add 1 to startdate
+						//var test = v_system.statuslines[j - 1].end + 1;
+						
+						syslines[i].statuslines.push({"start":(v_system.statuslines[j - 1].end), "end":(v_status.start - 1), "status":"available"});
+						syslines[i].statuslines.sort(custom_sort);
+						
+					}
+
+					//TODO: add free element after last?
+					/*
+					if(j + 1 >= syslines[i].statuslines.length){
+						console.log(v_status)
+					}
+					*/
+				
+				})
+
+			});
+
+		});
+					
+					
+
+		
+		return syslines;
+	
+	}
+	
+	
+	$scope.systemlines = getDBdata();
+	
 
 	$scope.selectedStatusLine = {
 		system:"", 
@@ -14,6 +66,7 @@ function TimelineCtrl($scope, Systems) {
 		end:undefined,
 	}
 
+	
 	$scope.calendar = {
 		daysLabels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
 		monthsLabels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
@@ -21,16 +74,17 @@ function TimelineCtrl($scope, Systems) {
 		current_date: new Date(),
 	}
 	
-    $scope.systemlines = Systems.query();
 	
 	function getSystemLines(){
 		var systemLines = Systems.query();	
 		
 	}
 	
+	
 	$scope.addTimeline = function() {
 		$scope.statuslines.push({system:$scope.system, start:$scope.start, end:$scope.end, status:$scope.status});
 	};
+	
 	
 	$scope.removeStatusElement = function() {
 	
@@ -44,20 +98,25 @@ function TimelineCtrl($scope, Systems) {
 						v_status.end == $scope.selectedStatusLine.end && 
 							v_status.status == $scope.selectedStatusLine.status){
 							
-								console.log("t1");	
-								//delete $scope.systemlines[i].statuslines[j];
-								$scope.systemlines[i].statuslines.splice(j,1);
-								console.log("t2");
-								//return;
-					}
-					
-				})
-				
-			}
-			
+								//$scope.systemlines[i].statuslines.splice(j,1);
+								//$scope.systemlines[i].statuslines[j].start = "";
+								//$scope.systemlines[i].statuslines[j].end = "";
+								$scope.systemlines[i].statuslines[j].status = "available";
+								
+								/*
+								//console.log("pr.status: " + $scope.systemlines[i].statuslines[j - 1].status);
+								if (j > 0 && $scope.systemlines[i].statuslines[j - 1].status == "available")
+								{
+									//console.log("v_status.start: " + v_status.end);
+									//console.log("$scope.systemlines[i].statuslines[j - 1].end: " + $scope.systemlines[i].statuslines[j - 1].end);
+									$scope.systemlines[i].statuslines[j - 1].end = (v_status.end);
+								}*/
+					}					
+				})				
+			}		
 		});
-
 	};
+  
   
     $scope.showDetails = function(system, statusline) {
 		$scope.selectedStatusLine.system = system;
@@ -65,6 +124,7 @@ function TimelineCtrl($scope, Systems) {
 		$scope.selectedStatusLine.start = statusline.start;
 		$scope.selectedStatusLine.end = statusline.end;
 	}
+  
   
 	$scope.getSystemLines = function() {
 	
@@ -82,9 +142,7 @@ function TimelineCtrl($scope, Systems) {
 				console.log(item + ' ' + val.system + ' ' + val.statuslines[0].start);
 
 			}
-		);
-		
-		
+		);	
 
 		console.log(list.length);
 		return list;
--- client/js/mongolab.js
+++ client/js/mongolab.js
@@ -2,7 +2,19 @@
 	
 angular.module('mongolab', ['ngResource']).
 	factory('Systems', function($resource) {
-		var Systems = $resource('https://api.mongolab.com/api/1/databases/saa_testdb/collections/system?apiKey=4fd9cdade4b05cb78ca54269');
+	
+		var Systems = $resource('https://api.mongolab.com/api/1/databases/saa_testdb/collections/system/:sys?apiKey=4fd9cdade4b05cb78ca54269', {sys:'@system'});	
+		//var Systems = $resource('https://api.mongolab.com/api/1/databases/saa_testdb/collections/system?apiKey=4fd9cdade4b05cb78ca54269');
+		
+		/*var test = Systems.get({sys:'P03'}, function(u, getResponse){
+						u.abc = true;
+						console.log('t10');
+						console.log(u);
+						console.log(getResponseHeaders);
+					});*/
+					
+		//console.log("asd: " + test);
+		
 		return Systems;
 	});
 	
