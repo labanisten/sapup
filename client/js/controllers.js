694259ebec5470ac5a7775a724451b41dd1094db
Jingart <jan.ingart@gmail.com>
2012-06-18 15:00:52 +0200

    addfunction

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,6 +4,33 @@ var myModule = angular.module('systemAvailability', ['mongolab']);
 
 function TimelineCtrl($scope, Systems) {
 
+
+	$scope.systemlines = getDBdata();
+	
+
+	$scope.selectedStatusLine = {
+		system:"", 
+		status:"", 
+		start:undefined, 
+		end:undefined,
+	}
+	
+	
+	$scope.addLine = {
+		system:"", 
+		status:"", 
+		start:undefined, 
+		end:undefined,
+	}
+
+	
+	$scope.calendar = {
+		daysLabels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		monthsLabels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		daysInMonth: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+		current_date: new Date(),
+	}
+	
 	
 	function custom_sort(a, b) {
 		//return new Date(a.start).getTime() - new Date(b.start).getTime();
@@ -30,62 +57,46 @@ function TimelineCtrl($scope, Systems) {
 
 		// TODO: sorting 
 		
-		var startdate = 20120501;
+		var startDate = 20120501;
+		var endDate = 20120530;
 
 		var syslines = Systems.query(function(){
+	
 
 			$.each(syslines, function(i, v_system) {
 			
 				$.each(v_system.statuslines, function(j, v_status) {
-
-					if(j == 0 && v_status.start > startdate){
+				
+					if(j == 0 && v_status.start > startDate){
 						
-						//var statusdate = convertToDate(v_status.start);
-						//var startdate = convertToDate('20120501');
-
-						for(k = 0; k < v_status.start - startdate; k++){
-						
-							//console.log(startdate + k);
-							syslines[i].statuslines.unshift({"start":startdate + k, "end":startdate + k, "status":"available"});
-							syslines[i].statuslines.sort(custom_sort);
-							
+						for(k = 0; k < v_status.start - startDate; k++){
+							syslines[i].statuslines.unshift({"start":startDate + k, "end":startDate + k, "status":"available"});
+							syslines[i].statuslines.sort(custom_sort);	
 						}
-						
-						//syslines[i].statuslines.unshift({"start":"20120501", "end":""+(v_status.start - 1)+"", "status":"available"});
-						
 						
 					}else if( j > 0 && v_status.start - 1 != (v_system.statuslines[j - 1].end)){
 					
-						//TODO: add 1 to startdate
-
 						var prev_end_date = parseInt(v_system.statuslines[j - 1].end) + 1;						
-						//console.log("m: " + (v_status.start - prev_end_date));
-						
-						for(k = 0; k < v_status.start - prev_end_date; k++){
-						
-							//console.log(parseInt(prev_end_date) + k);
-							//console.log(startdate + k);
-							syslines[i].statuslines.push({"start":prev_end_date + k, "end":prev_end_date + k, "status":"available"});
-							syslines[i].statuslines.sort(custom_sort);
-							
-						}
-						
-						//syslines[i].statuslines.push({"start":(v_system.statuslines[j - 1].end), "end":(v_status.start - 1), "status":"available"});
-						//syslines[i].statuslines.sort(custom_sort);
-						
-					}
 
-					//TODO: add free element after last?
-					/*
-					if(j + 1 >= syslines[i].statuslines.length){
-						console.log(v_status)
+						for(k = 0; k < v_status.start - prev_end_date; k++){
+							syslines[i].statuslines.push({"start":prev_end_date + k, "end":prev_end_date + k, "status":"available"});
+							syslines[i].statuslines.sort(custom_sort);		
+						}
 					}
-					*/
-				
 				})
 
+				var lastElementDate = parseInt(v_system.statuslines[v_system.statuslines.length - 1].end);
+				
+				if( lastElementDate < endDate){
+				
+					console.log("t1: " + (endDate - lastElementDate));
+					
+					lastElementDate += 1;
+					for(k = 0; k <= endDate - lastElementDate; k++){				
+							syslines[i].statuslines.push({"start":lastElementDate + k, "end":lastElementDate + k, "status":"available"});							
+					}
+				}
 			});
-
 		});
 					
 					
@@ -96,23 +107,7 @@ function TimelineCtrl($scope, Systems) {
 	}
 	
 	
-	$scope.systemlines = getDBdata();
-	
 
-	$scope.selectedStatusLine = {
-		system:"", 
-		status:"", 
-		start:undefined, 
-		end:undefined,
-	}
-
-	
-	$scope.calendar = {
-		daysLabels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-		monthsLabels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-		daysInMonth: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
-		current_date: new Date(),
-	}
 	
 	
 	function getSystemLines(){
@@ -137,7 +132,8 @@ function TimelineCtrl($scope, Systems) {
 					if(v_status.start == $scope.selectedStatusLine.start && 
 						v_status.end == $scope.selectedStatusLine.end && 
 							v_status.status == $scope.selectedStatusLine.status){
-
+								
+								//TODO: split line
 								$scope.systemlines[i].statuslines[j].status = "available";
 
 					}					
@@ -146,27 +142,60 @@ function TimelineCtrl($scope, Systems) {
 		});
 	};
 	
+			
+	$scope.addStatusElement = function() {
 	
-		$scope.addStatusElement = function() {
+		//console.log("start: " + $scope.addLine.start);
+		//console.log("end: " + $scope.addLine.end);
 	
 		$.each($scope.systemlines, function(i, v_system) {	
 		
-			if (v_system.system == $scope.selectedStatusLine.system) {
+			if (v_system.system == $scope.addLine.system) {
 			
+				//console.log("t1");
 				$.each(v_system.statuslines, function(j, v_status) {
 					
-					if(v_status.start == $scope.selectedStatusLine.start && 
-						v_status.end == $scope.selectedStatusLine.end && 
-							v_status.status == $scope.selectedStatusLine.status){
-							
+					
+					//TODO: add overlap if startdate is inside existing schedule
+					if(v_status.start == $scope.addLine.start && 
+						v_status.end == $scope.addLine.start){
+								
+								//console.log("t2");
 								//$scope.systemlines[i].statuslines.splice(j,1);
 								//$scope.systemlines[i].statuslines[j].start = "";							
 								//$scope.systemlines[i].statuslines[j].end = "";
 								
 								//$scope.systemlines[i].statuslines[j].status = "available";
-								if($scope.systemlines[i].statuslines[j].status == 'available'){
 								
-									$scope.systemlines[i].statuslines.push({"start":$scope.selectedStatusLine.start, "end":$scope.selectedStatusLine.end, "status":$scope.selectedStatusLine.status});
+								if($scope.addLine.start == $scope.addLine.end){
+									$scope.systemlines[i].statuslines[j].status = $scope.addLine.status;
+								}
+								else{
+								
+									//$scope.systemlines[i].statuslines.splice(j,1);
+									//console.log("tstart: " + $scope.addLine.start);
+									//console.log("tend: " + $scope.addLine.end);
+									//console.log("addl len: " + ($scope.addLine.end - v_status.start));
+									
+									for(k = 0; k < ($scope.addLine.end - v_status.start) + 1; k++){	
+										if($scope.systemlines[i].statuslines[j + k].status != 'available'){
+											alert("Overlap!!");
+											return false; //jquery break
+										}
+									}
+									
+
+									for(k = 0; k < ($scope.addLine.end - v_status.start) + 1; k++){	
+										//console.log("start: " + $scope.systemlines[i].statuslines[j].start);
+										//console.log("end: " + $scope.systemlines[i].statuslines[j].end);
+										//console.log("j: " + j + " k: " + k);
+										$scope.systemlines[i].statuslines.splice(j, 1);
+									}
+									
+									//syslines[i].statuslines.push({"start":prev_end_date + k, "end":prev_end_date + k, "status":"available"});
+									//syslines[i].statuslines.sort(custom_sort);		
+								
+									$scope.systemlines[i].statuslines.push({"start":$scope.addLine.start, "end":$scope.addLine.end, "status":$scope.addLine.status});
 									$scope.systemlines[i].statuslines.sort(custom_sort);
 									return false; //jquery break
 								}
