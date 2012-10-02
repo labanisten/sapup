7af8525a8cc9e09c57964fd4f6d496208c8f2896
Jingart <janingar@labanist.com>
2012-10-02 07:30:57 +0200

    static

--- client/js/mongodb.js
+++ client/js/mongodb.js
@@ -1,7 +1,7 @@
 angular.module('mongodbModule', []).
 	factory('db', function($http) {
 	
-		var DB_URL = "http://systemavailability.azurewebsites.net:27017/"; 
+		var DB_URL = "10.76.188.17:27017/"; 
 		var DB_NAME = "test";
 		//var DB_NAME = "test";
 		
