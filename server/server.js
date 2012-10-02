2b04d5fd12f91f9324555db8e48f389ab46f89ad
Jingart <janingar@labanist.com>
2012-10-02 10:40:18 +0200

    static

--- server/server.js
+++ server/server.js
@@ -3,22 +3,25 @@
 
 	var mongodb = require('mongodb');
 	var BSON = mongodb.BSONPure;
-    var dbServer = new mongodb.Server("168.63.58.169", 27017, {});
+    var dbServer = new mongodb.Server("centos-nosql-vm.cloudapp.net", 27017, {});
 	var db = new mongodb.Db('test', dbServer, {});
 	
+	/*
 	var allowCrossDomain = function(req, res, next) {
 		res.header('Access-Control-Allow-Origin', "*");
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		res.header('Access-Control-Allow-Headers', 'X-Requested-With');
 		next();
 	}
-	
+	*/
 	app.use(express.bodyParser({}));
-	app.use(allowCrossDomain);
+	//app.use(allowCrossDomain);
 	
 	//app.use("/client/js", express.static("/client/js"));
 	console.log("__dirname: " + __dirname);
 	app.use("/", express.static(__dirname + "/client/public"));
+	app.use("/images", express.static(__dirname + "/images"));
+	app.use("/css/images", express.static(__dirname + "/css/images"));
 	app.use("/js", express.static(__dirname + "/client/js"));
 	app.use("/css", express.static(__dirname + "/client/css"));
 	app.use("/bootstrap", express.static(__dirname + "/client/bootstrap"));
@@ -42,7 +45,8 @@
 	var restServices = {
 		get: function(req, res){
 				var resource = req.path.replace(/^\/|\/$/g, '');
-
+				console.log("req.path :" + req.path);
+				console.log("resource :" + resource);
 			    db.open(function(err, db) {
 					db.collection(resource, function(err, collection) {
 						collection.find().toArray(function(err, items) {
@@ -50,6 +54,7 @@
 							db.close();
 						});
 					});
+					if(err){console.log("open err :" + err);}
 			    });
 			 },
 		post:  function(req, res){
@@ -99,62 +104,35 @@
 	}
 
 	/*
-	app.get('/', function(req, res){
-	  res.send('REST server');
-	});*/
-	
-	/*
-	app.get('/css', function(req, res){
-		var uid = req.params.uid;
-		res.sendfile('./client/css/');
-	});
-	
-	app.get('/js', function(req, res){
-		var uid = req.params.uid;
-		res.sendfile('./client/js/');
-	});
-	
-	app.get('/bootstrap', function(req, res){
-		var uid = req.params.uid;
-		var path = req.params[0] ? req.params[0] : 'index.html';
-		res.sendfile('./client/public/' + path);
-	});
-	
-	app.get('/*', function(req, res){
-		var uid = req.params.uid;
-		var path = req.params[0] ? req.params[0] : 'index.html';
-		res.sendfile('./client/public/' + path);
-	});
-	*/
-	
 	app.all('/*', function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
 	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	  next();
 	});
+	*/
 	
 	app.get('/systems', function(req, res) {restServices.get(req, res);});
-	app.get('/systemname', function(req, res) {restservices.get(req, res);});
-	app.get('/alerttype', function(req, res) {restservices.get(req, res);});
-	app.get('/systemstatus', function(req, res) {restservices.get(req, res);});
+	app.get('/systemnames', function(req, res) {restServices.get(req, res);});
+	app.get('/alerttypes', function(req, res) {restServices.get(req, res);});
+	app.get('/systemstatuses', function(req, res) {restServices.get(req, res);});
 	app.get('/alerts', function(req, res) {restServices.get(req, res);});
 	
 	app.post('/systems', function(req, res) {restServices.post(req, res);});
-	app.post('/systemname', function(req, res) {restServices.post(req, res);});
-	app.post('/alerttype', function(req, res) {restServices.post(req, res);});
-	app.post('/systemstatus', function(req, res) {restServices.post(req, res);});
+	app.post('/systemnames', function(req, res) {restServices.post(req, res);});
+	app.post('/alerttypes', function(req, res) {restServices.post(req, res);});
+	app.post('/systemstatuses', function(req, res) {restServices.post(req, res);});
 	app.post('/alerts', function(req, res) {restServices.post(req, res);});
 	
 	app.put('/systems/:id', function(req, res) {restServices.put(req, res);});
-	app.put('/systemname/:id', function(req, res) {restServices.put(req, res);});
-	app.put('/alerttype/:id', function(req, res) {restServices.put(req, res);});
-	app.put('/systemstatus/:id', function(req, res) {restServices.put(req, res);});
+	app.put('/systemnames/:id', function(req, res) {restServices.put(req, res);});
+	app.put('/alerttypes/:id', function(req, res) {restServices.put(req, res);});
+	app.put('/systemstatuses/:id', function(req, res) {restServices.put(req, res);});
 	app.put('/alerts/:id', function(req, res) {restServices.put(req, res);});
 	
 	app.delete('/systems/:id', function(req, res) {restServices.delete(req, res);});
-	app.delete('/systemname/:id', function(req, res) {restServices.delete(req, res);});
-	app.delete('/alerttype/:id', function(req, res) {restServices.delete(req, res);});
-	app.delete('/systemstatus/:id', function(req, res) {restServices.delete(req, res);});
+	app.delete('/systemnames/:id', function(req, res) {restServices.delete(req, res);});
+	app.delete('/alerttypes/:id', function(req, res) {restServices.delete(req, res);});
+	app.delete('/systemstatuses/:id', function(req, res) {restServices.delete(req, res);});
 	app.delete('/alerts/:id', function(req, res) {restServices.delete(req, res);});
 	
 	function getResponse(error, result) {
