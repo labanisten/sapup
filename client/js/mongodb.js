3cd0a815f66be3f103aec83f8ec2306e4f74de88
Jingart <janingar@labanist.com>
2012-10-02 07:34:07 +0200

    static

--- client/js/mongodb.js
+++ client/js/mongodb.js
@@ -1,7 +1,7 @@
 angular.module('mongodbModule', []).
 	factory('db', function($http) {
 	
-		var DB_URL = "centos-nosql-vm.cloudapp.net:27017/"; 
+		var DB_URL = "http://centos-nosql-vm.cloudapp.net:27017/"; 
 		var DB_NAME = "test";
 		//var DB_NAME = "test";
 		
