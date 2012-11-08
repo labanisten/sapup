db1c53d9c6b7a5c00cb47ea5bc4f15a08aefd485
Jingart <janingar@labanist.com>
2012-11-08 10:11:05 +0100

    string

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -246,7 +246,6 @@ myModule.controller("TimelineCtrl", function($scope, db, Calendar, Utils) {
 			var alert = new db.Alert($scope.addAlertLine);
 			alert.expdate = Utils.viewDateToDBDate(alert.expdate);
 			var timestamp = new Date().getTime();
-			//timestamp = timestamp.toString();
 			alert.timestamp = timestamp;
 			alert.create().then(function(newAlert) {
 				$scope.alertlines.push(newAlert);
