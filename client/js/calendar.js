9c8d05ed1335f6732be1287d8bc8e0b1676fac02
Jingart <janingar@labanist.com>
2013-03-06 09:26:06 +0100

    refactor

--- client/js/calendar.js
+++ client/js/calendar.js
@@ -1,4 +1,5 @@
 "use strict";
+
 angular.module('calendarModule', []).
 	factory('Calendar', function() {
 
--- client/js/mongodb.js
+++ client/js/mongodb.js
@@ -1,4 +1,5 @@
 "use strict";
+
 angular.module('mongodbModule', []).
 	factory('db', function($http) {
 	
